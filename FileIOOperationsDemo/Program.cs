a5faec9d3add6f7096bd963ee482196663bd1f6a
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-09-15 18:04:54 +0530

    Add project files.

---
+++ FileIOOperationsDemo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOOperationsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("Welcome To FileIo Program!");
+            Console.WriteLine("--------------------------");
+            
+            //Functions Calling
+            FileExists();
+            ReadAllLines();
+            ReadAllText();
+
+            //Using Strem
+            StreamWriterOp streamWriterOp = new StreamWriterOp();
+            streamWriterOp.WriteUsingStreamWrite();
+            streamWriterOp.ReadFromStreamReader();
+        }
+
+        //Checking File Exists
+        public static void FileExists()
+        {
+            string path = @"C:\Users\DELL\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists : ");
+            }
+            //Console.ReadKey();
+        }
+
+        //Read File using ReadAllLines
+        public static void ReadAllLines()
+        {
+            string path = @"C:\Users\DELL\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+
+            string[] lines;
+            lines = File.ReadAllLines(path);
+            int count = lines.Length;
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(lines[i]);
+            }                        
+            //Console.ReadKey();
+        }
+        //Read File using ReadAllText
+        public static void ReadAllText()
+        {
+            string path = @"C:\Users\DELL\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+
+            string lines;
+            lines = File.ReadAllText(path);            
+            Console.WriteLine(lines);            
+            //Console.ReadKey();
+        }
+
+        public static void DeleteFile()
+        {
+            string path = @"C:\Users\DELL\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+
+            File.Delete(path);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ FileIOOperationsDemo/StreamWriterOp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileIOOperationsDemo
+{
+    public class StreamWriterOp
+    {
+        public void WriteUsingStreamWrite()
+        {
+            //FilePath
+            string path = @"C:\Users\DELL\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("Hello World - .net is Awesome ");
+                sw.Close();
+
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            Console.ReadKey();
+        }
+        //FilePath
+        public void ReadFromStreamReader()
+        {
+            string path = @"C:\Users\DELL\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+            using (StreamReader sr = File.OpenText(path))
+            {                
+                string s = "";
+
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }                
+            }
+            Console.ReadKey();
+        }
+    }
+}
